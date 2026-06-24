cc8708a147dc7b612dce147876986dc9014c2a4b
Petr Novák <petr.novak.gogo@gmail.com>
2026-06-24 20:09:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -426,16 +426,11 @@ async function switchToTimetable() {
         delayText = '+' + d.delay;
     }
 
-    let linkospoj = d.route;
-    if (p.attributes && p.attributes.cisjrLine && p.attributes.cisjrTrip) {
-        linkospoj = `${p.attributes.cisjrLine}/${p.attributes.cisjrTrip}`;
-    }
-
     let htmlContent = `
             <div class="tt-header" style="padding: 15px; margin: 0; background: #1e1e1e; border-bottom: 1px solid #333; z-index: 20;">
                 <button id="back-to-details-btn" class="tt-back-btn">Zpět</button>
                 <div class="tt-header-info">
-                    <span style="margin-right:15px;">Linkospoj: <strong>${linkospoj}</strong></span>
+                    <span style="margin-right:15px;">Linkospoj: <strong>${d.timetableRoute || d.route}</strong></span>
                     Zpoždění: <strong class="delay-val" style="color: ${delayColor}">${delayText}</strong>
                 </div>
             </div>
